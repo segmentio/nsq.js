904e31fd402ee92f0994402ee7b5169af77dd547
TJ Holowaychuk <tj@vision-media.ca>
2014-02-18 11:02:28 -0800

    fix example

--- examples/simple.js
+++ examples/simple.js
@@ -8,9 +8,7 @@ var nsq = require('..');
 // subscribe
 
 var reader = nsq.reader({
-  // nsqd: ['0.0.0.0:4150'],
-  // nsqd: ['0.0.0.0:4150', '0.0.0.0:4051', '0.0.0.0:4041'],
-  nsqlookupd: ['0.0.0.0:6000', '0.0.0.0:4161'],
+  nsqd: ['0.0.0.0:4150'],
   maxInFlight: 5,
   topic: 'events',
   channel: 'ingestion'
@@ -20,13 +18,13 @@ reader.on('message', function(msg){
   console.log(msg.id);
   setTimeout(function(){
     msg.finish();
-  }, 1500);
+  }, 200);
 });
 
 // publish
 
-// var writer = nsq.writer({ port: 4150 });
+var writer = nsq.writer({ port: 4150 });
 
-// setInterval(function(){
-//   writer.publish('events', 'some message here');
-// }, 500);
+setInterval(function(){
+  writer.publish('events', 'some message here');
+}, 150);

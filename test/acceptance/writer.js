c2c5ff7119f3ae020322cfdfb911040dd3c1185a
TJ Holowaychuk <tj@vision-media.ca>
2014-03-11 10:43:25 -0700

    add "error" event to writer

--- test/acceptance/writer.js
+++ test/acceptance/writer.js
@@ -28,8 +28,20 @@ describe('Writer#publish()', function(){
   it('should invoke callbacks with errors', function(done){
     var pub = nsq.writer({ port: 5000 });
 
+    pub.on('error', function(){});
+
     pub.publish('testing-writer', 'something', function(err){
       err.message.should.equal('no nsqd nodes connected');
+      done();
+    });
+  })
+
+  it('should emit "error"', function(done){
+    var pub = nsq.writer({ port: 5000 });
+
+    pub.on('error', function(err){
+      err.code.should.equal('ECONNREFUSED');
+      err.address.should.equal('0.0.0.0:5000');
       done();
     });
   })
